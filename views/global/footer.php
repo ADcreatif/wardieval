806513c9a533d57785bfbd30a7324611b8607aec
Hoolay <hoolay@free.fr>
2015-08-27 18:44:15 +0200

    pictures bg compression

--- views/global/footer.php
+++ views/global/footer.php
@@ -1,9 +1,9 @@
 <footer class="container-fluid">
     <div class="container">
-        <p>pied de page (des fois que vous ne l'auriez pas remarqué</p>
+        <p>pied de page (des fois que vous ne l'auriez pas remarqué)</p>
     </div>
 </footer>
 
-        <div id="dialog-confirm"></div>
-    </body>
+<div id="dialog-confirm"></div>
+</body>
 </html>
